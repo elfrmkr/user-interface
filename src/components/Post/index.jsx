f5ba006cc28d450840e76b7130d365a7c333f8bf
Elif Irem Kara Kadyrov <elfrmkr98@gmail.com>
2023-08-09 16:43:36 +0200

    feat: main page updates 1. added collapse functionality to the comments via icon click 2. added notification on top of the comment icon that shows how many comments a post have

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -10,7 +10,7 @@ import {
     TextField,
     Divider,
     List,
-
+    Collapse
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
@@ -39,29 +39,10 @@ const CommentText = styled(Typography)`
 `;
 
 const Post = ({ post, currentUser }) => {
-    const [comments, setComments] = useState([
-        {
-            text: 'This is a comment.',
-            date: '2023-07-27',
-            author: {
-                name: 'John Doe',
-                avatar: 'https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=912&q=80'
-            },
-            postId: "1",
-
-        },
-        {
-            text: 'This is a new random comment.',
-            date: '2023-09-27',
-            author: {
-                name: 'jane Doe',
-                avatar: 'https://plus.unsplash.com/premium_photo-1689551671541-31a345ce6ae0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80'
-            },
-            postId: "2"
-        },
-    ]);
+    const [comments, setComments] = useState([...post.comments]);
     const [newComment, setNewComment] = useState('');
     const [likeClicked, setLikeClicked] = useState(false);
+    const [showComments, setShowComments] = useState(false);
     const [dislikeClicked, setDislikeClicked] = useState(false);
 
     const handleLike = () => {
@@ -126,7 +107,13 @@ const Post = ({ post, currentUser }) => {
 
 
     return (
-        <Card sx={{ maxWidth: 600, margin: '16px auto', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
+        <Card sx={{
+            maxWidth: 900,
+            margin: '16px auto',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            borderRadius: '8px', // Add rounded corners
+
+        }}>
             <CardContent>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
                     <Avatar alt={post.author.name} src={post.author.avatar} />
@@ -156,13 +143,30 @@ const Post = ({ post, currentUser }) => {
                 <IconButton aria-label="dislike" onClick={handleDislike} color={dislikeClicked ? 'primary' : 'default'}>
                     <ThumbDownIcon />
                 </IconButton>
-                <IconButton aria-label="comment">
+                <IconButton aria-label="comment"
+                    onClick={() => setShowComments(!showComments)}>
                     <CommentIcon />
+                    {comments.length > 0 && (
+                        <span
+                            style={{
+                                position: 'absolute',
+                                top: '-8px',
+                                right: '-8px',
+                                backgroundColor: 'red',
+                                color: 'white',
+                                borderRadius: '50%',
+                                padding: '2px 6px',
+                                fontSize: '12px',
+                            }}
+                        >
+                            {comments.length}
+                        </span>
+                    )}
                 </IconButton>
             </CardActions>
             <Divider />
 
-            <div style={{ padding: '16px' }}>
+            <div style={{ padding: '16px', display: 'flex', justifyContent: "center", alignItems: "center" }}>
                 <TextField
                     label="Add a comment"
                     variant="outlined"
@@ -170,43 +174,45 @@ const Post = ({ post, currentUser }) => {
                     value={newComment}
                     onChange={(e) => setNewComment(e.target.value)}
                 />
-                <Button variant="contained" color="primary" onClick={handleAddComment} style={{ marginTop: '8px' }}>
+                <Button sx={{ marginLeft: 3 }} variant="contained" color="primary" onClick={handleAddComment} >
                     Comment
                 </Button>
             </div>
-            <List style={{ padding: -4, maxHeight: '300px', overflowY: 'auto' }}>
-                {comments.map((comment, index) => (
-                    (comment && comment.postId === post.postId) || !comment.postId ? (
-                        <CommentCard key={index} sx={{ background: '#eae7e9' }}>
-                            <CommentContent>
-                                <CommentAvatar alt={comment?.author?.name || ''} src={comment?.author?.avatar || ''} />
-                                <div>
-                                    <Typography variant="subtitle2">{comment?.author?.name || 'Anonymous'}</Typography>
-                                    <Typography variant="caption">{comment?.date || new Date().toLocaleDateString()}</Typography>
-                                </div>
-                            </CommentContent>
-                            <div style={{
-                                display: 'flex', justifyContent: "space-between", alignItems: "center"
-                            }}>
-                                <CommentText variant="body2" marginLeft={'18px'}>{comment?.text || comment}</CommentText>
-                                <CardActions>
-                                    <IconButton
-                                        fontSize="small"
-                                        aria-label="like" onClick={() => handleCommentLike(index)}>
-                                        <ThumbUpIcon color={comment?.likeClicked ? 'primary' : 'default'} />
-                                    </IconButton>
-                                    <IconButton
-                                        fontSize="small"
-                                        aria-label="dislike" onClick={() => handleCommentDislike(index)}>
-                                        <ThumbDownIcon color={comment?.dislikeClicked ? 'primary' : 'default'} />
-                                    </IconButton>
-                                </CardActions>
-                            </div>
-                        </CommentCard>
-                    ) : null
-                ))}
-            </List>
+            <Collapse in={showComments}>
 
+                <List style={{ padding: 16, maxHeight: '300px', overflowY: 'auto' }} >
+                    {comments.map((comment, index) => (
+                        (comment && comment.postId === post.postId) || !comment.postId ? (
+                            <CommentCard key={index} sx={{ background: '#eae7e9' }}>
+                                <CommentContent>
+                                    <CommentAvatar alt={comment?.author?.name || ''} src={comment?.author?.avatar || ''} />
+                                    <div>
+                                        <Typography variant="subtitle2">{comment?.author?.name || 'Anonymous'}</Typography>
+                                        <Typography variant="caption">{comment?.date || new Date().toLocaleDateString()}</Typography>
+                                    </div>
+                                </CommentContent>
+                                <div style={{
+                                    display: 'flex', justifyContent: "space-between", alignItems: "center"
+                                }}>
+                                    <CommentText variant="body2" marginLeft={'18px'}>{comment?.text || comment}</CommentText>
+                                    <CardActions>
+                                        <IconButton
+                                            fontSize="small"
+                                            aria-label="like" onClick={() => handleCommentLike(index)}>
+                                            <ThumbUpIcon color={comment?.likeClicked ? 'primary' : 'default'} />
+                                        </IconButton>
+                                        <IconButton
+                                            fontSize="small"
+                                            aria-label="dislike" onClick={() => handleCommentDislike(index)}>
+                                            <ThumbDownIcon color={comment?.dislikeClicked ? 'primary' : 'default'} />
+                                        </IconButton>
+                                    </CardActions>
+                                </div>
+                            </CommentCard>
+                        ) : null
+                    ))}
+                </List>
+            </Collapse>
         </Card >
     );
 };
